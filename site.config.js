536aec4655e9214bb52a86435382e5660928ead1
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-11-25 17:33:10 +0000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'bd6c9389a9804b37b71914729f73903c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'More Mussa',
+  domain: 'moremussa.com',
+  author: 'Mohamud Mussa',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: 'Hello World. Im Mohamud, an entrepreneur & website developer who does things sometimes and occasionally writes about them',
+  socialImageTitle: 'More Mussa',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'moremussa',
+  github: 'mohamud-mussa',
+  linkedin: 'mohamudmussa',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
